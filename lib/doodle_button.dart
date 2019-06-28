f24bb587a9e8deb9bbe1b720948de538a0a56606
jishaofeng89 <1195957057@qq.com>
2019-06-28 21:41:25 +0800

    first commit

---
+++ lib/doodle_button.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DoodleButton extends StatefulWidget {
+
+  @override
+  _DoodleButtonState createState() => _DoodleButtonState();
+}
+
+class _DoodleButtonState extends State<DoodleButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('xxx')),
+      body: Text('hhhhh'),
+    );
+  }
+}
